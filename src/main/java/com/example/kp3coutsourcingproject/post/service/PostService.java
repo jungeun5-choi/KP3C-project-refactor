2e98842fa8a36b9522c41aab4be74e97b0e69a7b
jungeun-choi <jungeun5.choi@gmail.com>
2023-07-28 13:59:53 +0900

    [Update] RedisCacheConfig로 HomeFeed 캐싱 추가

--- src/main/java/com/example/kp3coutsourcingproject/post/service/PostService.java
+++ src/main/java/com/example/kp3coutsourcingproject/post/service/PostService.java
@@ -8,6 +8,7 @@ import com.example.kp3coutsourcingproject.user.entity.User;
 import com.example.kp3coutsourcingproject.user.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,12 +25,15 @@ public class PostService {
 	private final UserRepository userRepository;
 
 	// 모든 글 계층형으로 조회(답글까지)
+	@Transactional
 	public List<PostResponseDto> getAllPosts() {
 		List<Post> postList = postRepository.findAllPosts();
 		return convertNestedStructure(postList);
 	}
 
 	// 홈피드(유저 작성 글 + 유저가 팔로잉한 글)
+	@Transactional
+	@Cacheable(value = "Home_Feed", key = "#user.username", cacheManager = "redisCacheManager")
 	public List<PostResponseDto> getHomeFeed(User user) {
 		List<PostResponseDto> posts = postRepository.getHomeFeed(user.getId()).stream()
 				.map(PostResponseDto::new)
@@ -39,6 +43,7 @@ public class PostService {
 	}
 
 	// 자기피드(유저 작성 글만)
+	@Transactional
 	public List<PostResponseDto> getMyFeed(User user) {
 		List<PostResponseDto> posts = postRepository.getUserFeed(user.getId()).stream()
 				.map(PostResponseDto::new)
@@ -48,6 +53,7 @@ public class PostService {
 	}
 
 	// 선택한 게시글에 대한 모든 답글 조회(답글의 답글 x, 답글만!)
+	@Transactional
 	public List<PostResponseDto> getChildPosts(Long id) {
 		List<PostResponseDto> childPosts = postRepository.findAllByParent(findPost(id)).stream()
 				.map(PostResponseDto::new)
@@ -58,6 +64,7 @@ public class PostService {
 	//////////////////////////////////////////////////////////////////////////
 
 	// 포스트 작성
+	@Transactional
 	public PostResponseDto createPost(PostRequestDto requestDto, User user) {
 		User targetUser = findUser(user.getId());
 
@@ -98,6 +105,7 @@ public class PostService {
 	}
 
 	// 포스트 삭제
+	@Transactional
 	public void deletePost(Long id, User user) {
 		Post post = findPost(id);
 		User targetUser = findUser(user.getId());
---
+++ src/main/java/com/example/kp3coutsourcingproject/redis/config/RedisCacheConfig.java
@@ -0,0 +1,32 @@
+package com.example.kp3coutsourcingproject.redis.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory cacheConnectionFactory) {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(10)); // 캐시 지속 시간
+
+        return RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(cacheConnectionFactory)
+                .cacheDefaults(configuration)
+                .build();
+    }
+}
